232723f7d3b7bd560b5dc4ff6efc853ea17c6efc
Elliot Landon <elliotlandon5@gmail.com>
2022-05-06 16:53:15 -0700

    began creating chat componenet

--- client/src/Components/Chat/index.jsx
+++ client/src/Components/Chat/index.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import io from 'socket.io-client';
+import ChatBox from './Chat'
+
+const socket = io.connect("http://localhost:3000");
 
 const ChatContainer = styled.div`
   flex-grow: 1;
   border: 1px solid black;
 `;
 
-function Chat({}) {
+function Chat({ }) {
+
+  let username = 'Elliot'
+
+  const joinRoom = () => {
+    socket.emit("join_room", username)
+  }
+
+  useEffect(() => {
+    joinRoom();
+  }, [])
+
   return (
     <ChatContainer>
-      chat
+
     </ChatContainer>
   )
 }
--- server/index.js
+++ server/index.js
@@ -2,16 +2,40 @@ const express = require('express');
 const path = require('path');
 const expressStaticGzip = require('express-static-gzip');
 const cors = require('cors');
+const http = require('http');
+const { Server } = require ('socket.io')
 // const Routers = require('./router');
 // const db = require('../database');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ["GET", "POST"],
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("User Connected", socket.id);
+
+  socket.on("join_room", (data) => {
+    socket.join(data)
+    console.log(data, 'has joined the room')
+  })
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id)
+  });
+});
+
 app.use(express.json());
 app.use(expressStaticGzip(`${__dirname}/../client/dist`));
 app.use(cors());
-// app.use('/', Routers);
+// // app.use('/', Routers);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
@@ -25,6 +49,6 @@ app.get('*.js', (req, res, next) => {
   next();
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
