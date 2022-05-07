9dcf1293cefd910f284b75124112e79521409816
Elbert Chan <elb.chan@gmail.com>
2022-05-07 15:03:33 -0700

    add skeleton to Details.jsx, add image property to monster data structure

--- client/src/Components/MonsterList/Details.jsx
+++ client/src/Components/MonsterList/Details.jsx
@@ -1,9 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import sampleArray from './../../exampleData/data';
+import Icons from './create/Icons';
+
+const DetailsContainer = styled.div`
+  margin: 2% 1% 1%;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid green;
+  width: 95%;
+  z-index: 100;
+`;
 
 function Details({}) {
+  const [monster, setMonster] = useState(sampleArray.Zelroth[0]);
+
   return (
-    <div>Details</div>
+    <DetailsContainer>
+      <img src={`./../assets/monsters/icons/${monster.image}`} alt="something" />
+      <div>Name: {monster.name}</div>
+      <div>Description: {monster.desscription}</div>
+      <div>Health: {monster.currentHealth}/{monster.maxHealth}</div>
+      <div>Armor Rating: {monster.armor}</div>
+      <div>Movement: {monster.movement}</div>
+      <div>
+        <div>Attacks</div>
+        {monster.attacks.map((e) => (
+          <>
+            <div>
+              Name: {e.attackName}
+            </div>
+            <div>
+              Attack: {e.multiplier > 1 ? `(` : ''}{e.attack}{e.multiplier > 1 ? `)* ${e.multiplier}` : ''}
+            </div>
+            <div>
+              Damage: {e.damage}
+            </div>
+          </>
+        )
+        )}
+      </div>
+
+    </DetailsContainer>
   );
 }
 
