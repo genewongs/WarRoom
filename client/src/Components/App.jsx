0b9bce518c3d4ed19e112aba8c1c4d91359f143f
Elliot Landon <elliotlandon5@gmail.com>
2022-05-10 16:01:31 -0700

    set up room context, added socket to board

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { signOut } from 'firebase/auth';
@@ -11,8 +11,10 @@ import Chat from './Chat/index';
 import BoardComponent from './Board/index';
 import Authentication from './Authentication/index';
 import UserContext from './UserContext';
+import io from 'socket.io-client';
 import ProtectedRoute from './Authentication/ProtectedRoute';
 import { Button } from '@mui/material';
+import RoomContext from './RoomContext';
 
 const AppContainer = styled.div`
   margin: 0px 100px 0px 100px;
@@ -37,7 +39,7 @@ const MasterContainer = styled.div`
   min-width: 100%;
 `;
 
-const logout = async ()=> {
+const logout = async () => {
   await signOut(auth);
 };
 
@@ -62,9 +64,16 @@ function MainHome() {
 function App() {
   const [currentUser, setCurrentUser] = useState({});
   console.log('currentUser in app', currentUser);
+  const socket = io.connect('http://localhost:3000');
   // console.log('currentUser !== {}', currentUser !== {});
 
-  useEffect(()=>{
+  const room = 123;
+
+  const joinRoom = () => {
+    socket.emit('join_room', room);
+  };
+
+  useEffect(() => {
     if (currentUser.currentUser !== undefined) {
       setCurrentUser(currentUser)
     } else {
@@ -74,14 +83,16 @@ function App() {
 
   return (
     <UserContext.Provider value={{ currentUser, setCurrentUser }}>
-      <Router>
-        <Routes>
-          {/* <Route element={<ProtectedRoute/>}> */}
-            <Route exact path="/" element={MainHome()}/>
-          {/* </Route> */}
-          <Route exact path="/login" element={<Authentication />} />
-        </Routes>
-      </Router>
+      <RoomContext.Provider value={ {joinRoom, room, socket} }>
+        <Router>
+          <Routes>
+            {/* <Route element={<ProtectedRoute/>}> */}
+            <Route exact path="/" element={MainHome()} />
+            {/* </Route> */}
+            <Route exact path="/login" element={<Authentication />} />
+          </Routes>
+        </Router>
+      </RoomContext.Provider>
     </UserContext.Provider>
   )
 }
--- server/index.js
+++ server/index.js
@@ -39,6 +39,10 @@ io.on('connection', (socket) => {
     socket.to(data.board).emit('recieve_log_message_data', data);
   });
 
+  socket.on('send_new_board', (newBoardSend) => {
+    socket.to(newBoardSend.room).emit('recieve_new_board', newBoardSend);
+  });
+
   socket.on('disconnect', () => {
     // console.log("User Disconnected", socket.id)
   });
