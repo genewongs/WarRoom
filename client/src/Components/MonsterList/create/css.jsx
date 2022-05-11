c5c93c33946dbd43a9bb303e0f405e012ddffa0f
Adarshraj Ravindran <adarshrajrav@gmail.com>
2022-05-11 10:01:18 -0700

    change create attack height

--- client/src/Components/MonsterList/create/css.jsx
+++ client/src/Components/MonsterList/create/css.jsx
@@ -72,7 +72,7 @@ const CSS = {
       &::-webkit-scrollbar {
       display: none;
       }
-      max-height: 234px;
+      max-height: 270px;
       h4 {
         font-weight: 250;
         font-size: 1em;
