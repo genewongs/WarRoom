744dc6fd3536dec8a96cdd135d2e01077fbc8e5a
Gene <gene.h.wong@gmail.com>
2022-06-01 18:02:13 -0500

    Update index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,19 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
-// import { BrowserRouter as Router , Routes, Route, useNavigate } from 'react-router-dom';
-
-// let RoutedApp = ({}) => {
-
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<App loadPage={"Home"}/>} />
-//         <Route path="/Search" element={<App loadPage={"Search"}/>} />'
-//         <Route path="/Saved" element={<App loadPage={"Saved"}/>} />'
-//       </Routes>
-//     </Router>
-//   )
-// }
 
 ReactDOM.render(<App />, document.getElementById('WarRoom'));
